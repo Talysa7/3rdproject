5518fc292845e6eb633720cbebf3181be5bbdb10
hyewon3701 <hyewon3701@users.noreply.github.com>
2018-10-30 16:36:21 +0900

    Revert "member, country, region 여기까지 함"
    This reverts commit d28b3defb9d004508e070b9c4160562ce696c9f8.

--- src/main/java/Temp/CountryDBBean.java
+++ src/main/java/Temp/CountryDBBean.java
@@ -1,16 +1,5 @@
 package Temp;
 
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-
-import bean.SqlMapClient;
-import Temp.BoardDataBean;
-
 public class CountryDBBean {
-	private SqlSession session=SqlMapClient.getSession();
-	
-	public List<BoardDataBean> selectCountry(int b_no) {
-		return session.selectList("location.selectCountry",b_no);
-	}
+
 }
