118dd3776090d6826a488c6802461cb52ef32fe3
leoh7 <leoh7@naver.com>
2018-10-30 18:06:36 +0900

    CmtDBBean 내 매개변수 수정 c_ => cmt_ 로 교체

--- src/main/java/Temp/CmtDBBean.java
+++ src/main/java/Temp/CmtDBBean.java
@@ -10,17 +10,16 @@ import Temp.CmtDataBean;
 
 public class CmtDBBean {
 	SqlSession session = SqlMapClient.getSession();
-	// TODO : 1. CmtDBBean c_ 네이밍 된 컬럼명 바꾸는 것 항시 체크 / xml의 쿼리 포함
+	// TODO : 1. CmtDBBean 중 c_ 네이밍 된 컬럼명 바꾸는 것 항시 체크 / xml의 쿼리, 핸들러 등등
 	// TODO : 2. xml 내 mapper namespace 주는것 체크
 	
 	//total comment
 	public int insertComment(CmtDataBean cmtDto) {
-		// TODO : CmtDBBean c_id 시퀀스 없는지?
+		// TODO : CmtDBBean comment_id 시퀀스 없는지?
 		return session.insert("board.insertComment", cmtDto);
 	}
 	
 	public List<CmtDataBean> getComment( int board_no ) {
-		// FIXME : CmtDBBean tb_no 가 바뀐게 b_no 인것 같으니  확인 필요
 		return session.selectList("board.getComment", board_no);
 	}
 	
@@ -29,8 +28,8 @@ public class CmtDBBean {
 		return session.update("board.updateComment", cmtDto);
 	}
 	
-	public int deleteComment( int c_id ) {
-		return session.delete("board.deleteComment", c_id);
+	public int deleteComment( int comment_id ) {
+		return session.delete("board.deleteComment", comment_id);
 	}
 	
 	public int getCmtCount() {
