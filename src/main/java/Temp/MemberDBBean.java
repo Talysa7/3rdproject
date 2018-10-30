54e10c7e45280491d93ebe0f666eca7f6ace0a60
hyewon3701 <hyewon3701@users.noreply.github.com>
2018-10-30 16:46:02 +0900

    Revert "Revert "member, country, region 여기까지 함""
    This reverts commit 5518fc292845e6eb633720cbebf3181be5bbdb10.

--- src/main/java/Temp/MemberDBBean.java
+++ src/main/java/Temp/MemberDBBean.java
@@ -1,5 +1,30 @@
 package Temp;
 
-public class MemberDBBean {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+
+import bean.SqlMapClient;
+import Temp.UserDataBean;
+
+public class MemberDBBean {
+	private SqlSession session=SqlMapClient.getSession();
+
+	public List<UserDataBean> getCurrentMember(String pao_trip_id) {
+		List<UserDataBean> memberList=session.selectList("user.getCurrentMember", pao_trip_id);
+		for(UserDataBean user:memberList) {
+			user.setUser_name((String)session.selectOne("user.getUserName", user.getUser_id()));
+		}
+		return memberList;
+	}
+	public int addTripMember(Map<String, String> addMemberMap) {
+		return session.update("user.addTripMember", addMemberMap);
+	}	
+	public int delTripMember(Map<String, String> delMemberMap) {
+		return session.update("user.delTripMember", delMemberMap);
+	}	
+	public int isMember(Map<String, String> delMemberMap) {
+		return session.selectOne("user.isMember2", delMemberMap);
+	}
 }
