29b1b0e6d30ac4ed3339577cb6ab7e300f59c585
Lazy Talysa <42195207+Talysa7@users.noreply.github.com>
2018-10-31 18:58:53 +0900

    Trip refactoring (30%)
    add, remove, and fix methods of TripDataBean and TripDBBean

--- src/main/java/Temp/TripDataBean.java
+++ src/main/java/Temp/TripDataBean.java
@@ -4,13 +4,13 @@ import java.sql.Date;
 
 //database table : pao_trip
 public class TripDataBean {
-	private int trip_id;				//int (10), PK, not null, on delete cascade
-	private int trip_m_count;			//smallint (4), not null
-	private Date start_date;			//datetime, not null
-	private Date end_date;				//datetime, not null
+	private int trip_id;								//int (10), PK, not null, on delete cascade
+	private int trip_member_count;			//smallint (4), not null
+	private Date start_date;						//timestamp, not null
+	private Date end_date;						//timestamp, not null
 	//FK
-	private int b_no;					//int (10), not null
-	private int coord_id;				//int (8), not null
+	private int b_no;							//int (10), not null
+	private int coord_id;					//int (8), not null
 	
 	public int getTrip_id() {
 		return trip_id;
@@ -18,11 +18,11 @@ public class TripDataBean {
 	public void setTrip_id(int trip_id) {
 		this.trip_id = trip_id;
 	}
-	public int getTrip_m_count() {
-		return trip_m_count;
+	public int getTrip_member_count() {
+		return trip_member_count;
 	}
-	public void setTrip_m_count(int trip_m_count) {
-		this.trip_m_count = trip_m_count;
+	public void setTrip_m_count(int trip_member_count) {
+		this.trip_member_count = trip_member_count;
 	}
 	public int getCoord_id() {
 		return coord_id;
