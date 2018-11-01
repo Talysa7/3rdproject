d52279fef5c0ce2b5fa48ee445bc65f5219e0367
Lazy Talysa <42195207+Talysa7@users.noreply.github.com>
2018-11-01 16:50:50 +0900

    Update src/main/java/Temp/TripDataBean.java

--- src/main/java/Temp/TripDataBean.java
+++ src/main/java/Temp/TripDataBean.java
@@ -2,15 +2,18 @@ package Temp;
 
 import java.sql.Date;
 
+//database view(with user_name) : pao_view_trip
 //database table : pao_trip
 public class TripDataBean {
-	private int trip_id;								//int (10), PK, not null, on delete cascade
-	private int trip_member_count;			//smallint (4), not null
-	private Date start_date;						//timestamp, not null
-	private Date end_date;						//timestamp, not null
+	private int trip_id;										//int (10), PK, not null, on delete cascade
+	private int trip_member_count;					//smallint (4), not null
+	private Date start_date;								//timestamp, not null
+	private Date end_date;								//timestamp, not null
 	//FK
-	private int b_no;							//int (10), not null
-	private int coord_id;					//int (8), not null
+	private int b_no;											//int (10), not null
+	private int coord_id;									//int (8), not null
+	//guest value from pao_user
+	private String user_name;							//varchar (20), not null
 	
 	public int getTrip_id() {
 		return trip_id;
@@ -21,7 +24,7 @@ public class TripDataBean {
 	public int getTrip_member_count() {
 		return trip_member_count;
 	}
-	public void setTrip_m_count(int trip_member_count) {
+	public void setTrip_member_count(int trip_member_count) {
 		this.trip_member_count = trip_member_count;
 	}
 	public int getCoord_id() {
@@ -47,5 +50,11 @@ public class TripDataBean {
 	}
 	public void setB_no(int b_no) {
 		this.b_no = b_no;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
 	}
 }
