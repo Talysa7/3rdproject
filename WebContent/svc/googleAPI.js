17f9c091b81e1f14a4ed725f10563a4b71ccc557
leoh7 <leoh7@naver.com>
2018-12-07 18:21:05 +0900

    편의에 맞춰서 map API 손보는 중

--- WebContent/svc/googleAPI.js
+++ WebContent/svc/googleAPI.js
@@ -3,34 +3,37 @@ function initMap() {
 	var input = document.getElementById('pac-input');
 	var types = document.getElementById('type-selector');
 	var infowindowContent = document.getElementById('infowindow-content');
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center : {
-			lat : 37.4864,
-	        lng : 127.0207
-		},
-		zoom : 13
-	});
+	var zoom = 13
+	var after_zoom = 17
+	var map_opt = {
+			center : {
+				lat : 37.4864,
+		        lng : 127.0207
+			},
+			zoom : zoom
+		}
+	var map = new google.maps.Map(document.getElementById('map'), map_opt);
 	
-
 	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
 	var autocomplete = new google.maps.places.Autocomplete(input);
-
 	autocomplete.bindTo('bounds', map);
 	autocomplete.setFields([ 'address_components', 'geometry', 'icon', 'name' ]);
 
 	var infowindow = new google.maps.InfoWindow();
-
 	infowindow.setContent(infowindowContent);
+
 	var marker = new google.maps.Marker({
 		map : map,
 		anchorPoint : new google.maps.Point(0, -29)
 	});
+	
 //	마커 이벤트
-//	google.maps.event.addListener(marker,'click',function() {
-//		map.setZoom(16);
-//		map.setCenter(marker.getPosition());
-//	});
+	google.maps.event.addListener(marker,'click',function() {
+		after_zoom++
+		map.setZoom(after_zoom);
+		map.setCenter(marker.getPosition());
+	});
 	autocomplete.addListener('place_changed', function() {
 		infowindow.close();
 		marker.setVisible(false);
@@ -45,7 +48,7 @@ function initMap() {
 			map.fitBounds(place.geometry.viewport);
 		} else {
 			map.setCenter(place.geometry.location);
-			map.setZoom(17); 
+			map.setZoom(after_zoom); 
 		}
 		marker.setPosition(place.geometry.location);
 		marker.setVisible(true);
