d92bf05cdaedc8e6df734d1a0d7a8a14d50181fa
leoh7 <leoh7@naver.com>
2018-12-07 18:11:04 +0900

    map API 정리

--- WebContent/svc/googleAPI.js
+++ WebContent/svc/googleAPI.js
@@ -1,63 +1,51 @@
 function initMap() {
-	
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center : {
-			lat : -33.8688,
-			lng : 151.2195
-		},
-		zoom : 13
-	});
 	var card = document.getElementById('pac-card');
 	var input = document.getElementById('pac-input');
 	var types = document.getElementById('type-selector');
-	var strictBounds = document.getElementById('strict-bounds-selector');
+	var infowindowContent = document.getElementById('infowindow-content');
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center : {
+			lat : 37.4864,
+	        lng : 127.0207
+		},
+		zoom : 13
+	});
+	
 
 	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
 	var autocomplete = new google.maps.places.Autocomplete(input);
 
-	// Bind the map's bounds (viewport) property to the autocomplete object,
-	// so that the autocomplete requests use the current map bounds for the
-	// bounds option in the request.
 	autocomplete.bindTo('bounds', map);
-
-	// Set the data fields to return when the user selects a place.
 	autocomplete.setFields([ 'address_components', 'geometry', 'icon', 'name' ]);
 
 	var infowindow = new google.maps.InfoWindow();
-	var infowindowContent = document.getElementById('infowindow-content');
-	var schedule = document.getElementById('schedule');
+
 	infowindow.setContent(infowindowContent);
 	var marker = new google.maps.Marker({
 		map : map,
 		anchorPoint : new google.maps.Point(0, -29)
 	});
-	google.maps.event.addListener(marker,'click',function() {
-		map.setZoom(16);
-		map.setCenter(marker.getPosition());
-	});
+//	마커 이벤트
+//	google.maps.event.addListener(marker,'click',function() {
+//		map.setZoom(16);
+//		map.setCenter(marker.getPosition());
+//	});
 	autocomplete.addListener('place_changed', function() {
 		infowindow.close();
 		marker.setVisible(false);
 		var place = autocomplete.getPlace();
 		if (!place.geometry) {
-			// User entered the name of a Place that was not
-			// suggested and
-			// pressed the Enter key, or the Place Details
-			// request failed.
 			window.alert("찾으시는 '"+ place.name + "' 에 대한 결과가 없습니다 \n"
 			+ "다른 검색어를 입력 해주세요");
 			return;
 		}
 
-		// If the place has a geometry, then present it on a
-		// map.
 		if (place.geometry.viewport) {
 			map.fitBounds(place.geometry.viewport);
 		} else {
 			map.setCenter(place.geometry.location);
-			map.setZoom(17); // Why 17? Because it looks
-								// good.
+			map.setZoom(17); 
 		}
 		marker.setPosition(place.geometry.location);
 		marker.setVisible(true);
@@ -83,24 +71,5 @@ function initMap() {
 		$("#place"+num).val( place.name );
 		
 		infowindow.open(map, marker);
-	});
-
-	// Sets a listener on a radio button to change the filter type on Places
-	// Autocomplete.
-	function setupClickListener(id, types) {
-		var radioButton = document.getElementById(id);
-		radioButton.addEventListener('click', function() {
-			autocomplete.setTypes(types);
-		});
-	}
-
-	setupClickListener('changetype-all', []);
-
-	document.getElementById('use-strict-bounds')
-		.addEventListener('click', function() {
-			console.log('Checkbox clicked! New state=' + this.checked);
-			autocomplete.setOptions({
-				strictBounds : this.checked
-			});
 	});
 }
